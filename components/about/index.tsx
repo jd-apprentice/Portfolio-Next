9b01ed5c3f84da47061d4d818e379eb85b40b736
Jonathan <emeraldusk@gmail.com>
2022-07-21 01:01:29 -0300

    responsive button

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -15,7 +15,7 @@ const About = () => {
           <h3 className="text-2xl font-bold text-black-900 lg:text-3xl dark:text-white">
             {t("job")}
           </h3>
-          <div className="flex space-x-2 mt-2">
+          <div className="flex items-center justify-center md:justify-center lg:justify-start xsm:justify-center sm:justify-center space-x-2 mt-2">
             <button
               type="button"
               className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
