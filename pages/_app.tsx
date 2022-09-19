67971b2bc9ef7ae3d9abe9192b851c1b6c10ccb5
Jonathan Dyallo <emeraldusk@gmail.com>
2022-09-19 00:55:58 -0300

    static paths locales

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
-import 'tailwindcss/tailwind.css';
-import type { AppProps } from 'next/app';
-import { Footer, Navigation } from '../components';
-import { ThemeProvider } from 'next-themes';
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import { Footer, Navigation } from "../components";
+import { ThemeProvider } from "next-themes";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-  <>
-    <ThemeProvider attribute="class" enableSystem={false}>
-    <Navigation/>
-    <Component {...pageProps} />
-    </ThemeProvider>
-    <Footer />
-  </>
-  )
-}
+    <>
+      <ThemeProvider attribute="class" enableSystem={false}>
+        <Navigation />
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Footer />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -4,57 +4,79 @@ import matter from "gray-matter";
 import marked from "marked";
 import Link from "next/link";
 import { IPost } from "../../models";
+import Head from "next/head";
+import { config } from "../../config";
+// import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function PostPage({
-  frontmatter: { title, date, cover_image, alt },
+  frontmatter: { title, date, cover_image, alt, excerpt },
   content,
 }: IPost) {
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-start text-center">
-        <Link href="/blog">
-          <button
-            type="button"
-            className="me-5 mt-3 inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out dark:bg-blue-600 dark:hover:bg-blue-700 dark:active:bg-blue-800 dark:focus:bg-blue-700"
-          >
-            Go back
-          </button>
-        </Link>
-      </div>
-      <div>
-        {/* @ts-ignore */}
-        <div style={{ textAlign: "-webkit-center" }} className="text-center">
-          <h1 className="text-3xl">{title}</h1>
-          <div className="text-xl">Posted on {date}</div>
-          <img src={cover_image} alt={alt} />
+    <>
+      <Head>
+        <title>Jonathan Dyallo - Blog</title>
+        <link
+          rel="shortcut icon"
+          type="image/jpg"
+          href="https://raw.githubusercontent.com/jd-apprentice/Portfolio-Next/main/public/favicon.png"
+        />
+        <meta property="og:image" content={config.github.url} />
+        <meta name="description" content={excerpt}></meta>
+        <meta property="og:description" content={excerpt} />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="https://jonathandyallo.vercel.app" />
+      </Head>
+      <div className="container mx-auto">
+        <div className="flex justify-start text-center">
+          <Link href="/blog">
+            <button
+              type="button"
+              className="me-5 mt-3 inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out dark:bg-blue-600 dark:hover:bg-blue-700 dark:active:bg-blue-800 dark:focus:bg-blue-700"
+            >
+              Go back
+            </button>
+          </Link>
         </div>
-        <div className="text-base mx-auto mt-3 py-3">
-          {/* @ts-ignore */}
-          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        <div>
+          <div className="text-center">
+            <h1 className="text-3xl">{title}</h1>
+            <div className="text-xl">Posted on {date}</div>
+            <img src={cover_image} alt={alt} />
+          </div>
+          <div className="text-base mx-auto mt-3 py-3">
+            {/* @ts-ignore */}
+            <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export async function getStaticPaths() {
+export async function getStaticPaths({ locales }: { locales: string[] }) {
   const files = fs.readdirSync(path.join("posts"));
-  const paths = files.map((filename) => ({
-    params: {
-      slug: filename.replace(".md", ""),
-    },
-  }));
+  const paths = files.flatMap((filename) => {
+    return locales.map((locale) => {
+      return {
+        params: {
+          slug: filename.replace(".md", ""),
+        },
+        locale: locale,
+      };
+    });
+  });
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
 export async function getStaticProps({
   params: { slug },
 }: {
-  params: { slug: string };
+  params: { slug: string; locale: any };
 }) {
   const markdownWithMeta = fs.readFileSync(
     path.join("posts", slug + ".md"),
