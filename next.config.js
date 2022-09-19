83f9d67f6d1417b47fc0214ed0a5ff805906f749
Jonathan Dyallo <emeraldusk@gmail.com>
2022-09-18 22:56:09 -0300

    added blog

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextTranslate = require('next-translate')
-
 module.exports = nextTranslate({
   webpack: (config, { isServer, webpack }) => {
     return config;
