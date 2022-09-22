6857655415128b1d87bee9360795151d188855ee
Jonathan Dyallo <emeraldusk@gmail.com>
2022-09-22 13:37:59 -0300

    fix for config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 
+const withPlugins = require('next-compose-plugins');
 const nextTranslate = require('next-translate');
 const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = nextTranslate({
-  webpack: (config) => {
-    return config;
-  },
-  reactStrictMode: true,
-  images: {
-    domains: ["avatars.githubusercontent.com"]
-  }
-})
- 
-module.exports = withMT({
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
+
+module.exports = withPlugins([
+  // Taildwind Material Configuration
+  [withMT, {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }],
+
+  // Next Translate Configuration
+  [nextTranslate, {
+    webpack: (config) => {
+      return config;
+    },
+    reactStrictMode: true,
+    images: {
+      domains: ["avatars.githubusercontent.com"]
+    }
+  }],
+]);
