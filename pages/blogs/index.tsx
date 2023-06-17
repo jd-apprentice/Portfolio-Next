90fa70b84827e056ae80a96e08c9c21fcd1a6918
Jonathan Dyallo <emeraldusk@gmail.com>
2023-06-17 16:41:47 -0300

    fix(typing): types

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -11,7 +11,7 @@ interface Props {
                         slug: string;
                         coverImage: string;
                         dateAdded: string;
-                    };
+                    }[]
                 };
             }
         }
